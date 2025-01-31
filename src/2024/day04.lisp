2f0feb6643dcd88b4abcdd67ae2173841dd87897
Jonathan Trudeau <trudeaujt@gmail.com>
2025-01-31 11:04:32 +0900

    Work on day 4

--- src/2024/day04.lisp
+++ src/2024/day04.lisp
@@ -1,84 +1,49 @@
 (load "~/code/advent-of-code/src/2024/helpers.lisp")
 
-(defparameter small-input 
-"....XXMAS..
-.SAMXMS....
-...S..A....
-..A.A.MS.X.
-XMASAMX.MM.
-X.....XA.A.
-S.S.S.S.SS.
-.A.A.A.A.A.
-..M.M.M.MM.
-.X.X.XMASX.")
+(defparameter small-input #2A(
+(#\. #\. #\. #\. #\X #\X #\M #\A #\S #\. #\.)
+(#\. #\S #\A #\M #\X #\M #\S #\. #\. #\. #\.)
+(#\. #\. #\. #\S #\. #\. #\A #\. #\. #\. #\.)
+(#\. #\. #\A #\. #\A #\. #\M #\S #\. #\X #\.)
+(#\X #\M #\A #\S #\A #\M #\X #\. #\M #\M #\.)
+(#\X #\. #\. #\. #\. #\. #\X #\A #\. #\A #\.)
+(#\S #\. #\S #\. #\S #\. #\S #\. #\S #\S #\.)
+(#\. #\A #\. #\A #\. #\A #\. #\A #\. #\A #\.)
+(#\. #\. #\M #\. #\M #\. #\M #\. #\M #\M #\.)
+(#\. #\X #\. #\X #\. #\X #\M #\A #\S #\X #\.)))
+
+(defparameter tiny-input #2A(
+(#\X #\M #\A #\S)
+(#\. #\. #\. #\M)
+(#\. #\. #\. #\A)
+(#\. #\. #\. #\X)                             
+))
+
+(defparameter directions '(
+(-1 -1) (-1 0) (-1 1)
+ (0 -1)         (0 1)
+ (1 -1)  (1 0)  (1 1)))
 
 (defparameter xmas "XMAS")
 (defparameter samx "SAMX")
 
-(defparameter dummy-input 
-#((A B C)
-  (B C D)
-  (C D E)))
-
-"A
-BB
-CCC
-DD
-E"
-
-(defun rotate-right (text)
-    (let* ((vec (split-sequence:split-sequence #\newline text)) 
-           (height (length vec))
-           (width (length (first vec))))
-        (loop for x upto height
-                  do (loop for y upto width
-                           do (princ (aref text x))))
-        ))
-
-(rotate-right small-input)
-
-(defun rotate-string-45 (input-string)
-  (let* ((lines (split-sequence:split-sequence #\Newline input-string))
-         (rows (length lines))
-         (cols (length (first lines)))
-         (diagonal-count (+ rows cols -1))
-         (diagonals (make-array diagonal-count :initial-element nil)))
-    ;; Initialize diagonals as empty lists
-    (dotimes (i diagonal-count)
-      (setf (aref diagonals i) '()))
-    ;; Populate diagonals directly from the string
-    (dotimes (i rows)
-      (dotimes (j cols)
-        (when (< j (length (nth i lines)))  ; Avoid out-of-bounds
-          (let ((char (char (nth i lines) j))
-                (diag (+ i j)))
-            (push char (aref diagonals diag))))))
-    ;; Convert diagonals vector to a list of strings
-    (let ((result
-           (map 'list
-                (lambda (diag) (coerce (reverse diag) 'string))
-                diagonals)))
-      ;; Join the strings with newline characters
-      (apply #'concatenate 'string
-             (mapcan (lambda (str) (list str (string #\Newline))) result)))))
-
-;; Example usage
-(defparameter my-array
-"ABC
-BCD
-CDE")
-
-(format t "~A~%" (rotate-string-45 my-array))
-
 (defun part1 (text)
-    (let ((sum 0)) 
-        (labels ((count-xmas (line xmas) 
-                             (let ((idx (search xmas line))) 
-                                 (when idx 
-                                       (incf sum) 
-                                       (count-xmas (subseq line (+ 4 idx)) xmas))))) 
-            (count-xmas text "XMAS")
-            (count-xmas text "SAMX"))
+    (let* ((sum 0)
+           (dimensions (array-dimensions text))
+           (len (first dimensions))
+           (width (second dimensions)))
+        (dotimes (x len)
+            (dotimes (y width)
+                (dolist (dir directions)
+                    (let ((new-x (+ x (first dir)))
+                          (new-y (+ y (second dir))))
+                        (unless (or (> 0 new-x)
+                                    (> 0 new-y)
+                                    (> len new-x)
+                                    (> len new-y))
+                            (format t "Value at new index ~A x ~A, walking from ~A x ~A: ~A " new-x new-y x y (aref text new-x new-y))))))
+            (fresh-line))
         sum))
 
 (part1 small-input)
+(part1 tiny-input)
