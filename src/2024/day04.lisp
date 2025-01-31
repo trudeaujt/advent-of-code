2455d8150e76bb960d91e78b5bb34a3b97d4e568
Jonathan Trudeau <trudeaujt@gmail.com>
2025-02-01 02:43:46 +0900

    Solve day4 part1

--- src/2024/day04.lisp
+++ src/2024/day04.lisp
@@ -14,9 +14,9 @@
 
 (defparameter tiny-input #2A(
 (#\X #\M #\A #\S)
-(#\. #\. #\. #\M)
-(#\. #\. #\. #\A)
-(#\. #\. #\. #\X)                             
+(#\M #\. #\. #\A)
+(#\A #\. #\. #\M)
+(#\S #\. #\. #\X)
 ))
 
 (defparameter directions '(
@@ -25,7 +25,6 @@
  (1 -1)  (1 0)  (1 1)))
 
 (defparameter xmas "XMAS")
-(defparameter samx "SAMX")
 
 (defun part1 (text)
     (let* ((sum 0)
@@ -35,15 +34,25 @@
         (dotimes (x len)
             (dotimes (y width)
                 (dolist (dir directions)
-                    (let ((new-x (+ x (first dir)))
-                          (new-y (+ y (second dir))))
-                        (unless (or (> 0 new-x)
-                                    (> 0 new-y)
-                                    (> len new-x)
-                                    (> len new-y))
-                            (format t "Value at new index ~A x ~A, walking from ~A x ~A: ~A " new-x new-y x y (aref text new-x new-y))))))
-            (fresh-line))
+                        (let ((new-x (+ x (first dir)))
+                              (new-y (+ y (second dir)))
+                              (word (make-array 1
+                                        :element-type 'character 
+                                        :adjustable t 
+                                        :fill-pointer 1 
+                                        :initial-contents (list (aref text x y)))))
+                        (dotimes (iter 3)
+                            (unless (or (> 0 new-x)
+                                        (> 0 new-y)
+                                        (< (- len 1) new-x)
+                                        (< (- len 1) new-y))
+                                (vector-push-extend (aref text new-x new-y) word)
+                                (when debug-mode (format t "Value at new index ~A x ~A, walking from ~A x ~A: ~A [~A]~%" new-x new-y x y (aref text new-x new-y) word))
+                                (incf new-x (first dir))
+                                (incf new-y (second dir))))
+                        (if (string= xmas word) (incf sum)))))
+            (when debug-mode (fresh-line)))
         sum))
 
+(defparameter debug-mode t)
 (part1 small-input)
-(part1 tiny-input)
