7fbe9ea8e0608eb155782d13ee44a54af708c32c
Jonathan Trudeau <trudeaujt@gmail.com>
2025-02-27 22:15:25 +0900

    day05 work

--- src/2024/day05.lisp
+++ src/2024/day05.lisp
@@ -1,65 +1,48 @@
 (load "~/code/advent-of-code/src/2024/helpers.lisp")
 (load "~/sync/advent-of-code/src/2024/helpers.lisp")
 
-(defparameter tiny-input
- "47|53
-97|13
-97|61
-97|47
-75|29
-61|13
-75|53
-29|13
-97|29
-53|29
-61|53
-97|53
-61|29
-47|13
-75|47
-97|75
-47|61
-75|61
-47|29
-75|13
-53|13
+(defun split-string-on-double-newline (str)
+  (let ((delim '(#\Newline #\Newline)))
+    (let ((result '())
+          (start 0))
+      (loop for pos = (search delim str :start2 start)
+            while pos do
+            (push (subseq str start pos) result)
+            (setf start (+ pos (length delim))))
+      (push (subseq str start) result)
+      (nreverse result))))
 
-75,47,61,53,29
-97,61,53,29,13
-75,29,13
-75,97,47,61,53
-61,13,29
-97,13,75,29,47")
-
-(first (let ((sep-index (search "#\Newline#\Newline" tiny-input)))
-         (if sep-index
-             (list (subseq tiny-input 0 sep-index)
-                   (subseq tiny-input (+ sep-index 2)))
-             (list tiny-input))))
-
-(defparameter rule-table
-    (let ((rule-table (make-hash-table :test #'equal)))
-        (dolist (pair (mapcar (lambda (rule) 
-                                  (split-sequence:split-sequence #\| rule)) 
-                          (split-sequence:split-sequence #\Newline rules)))
-          (push (cadr pair) (gethash (car pair) rule-table)))
-       rule-table))
-
-(defparameter update-list
-    (let ((update-list (mapcar 
-                            (lambda (update) (split-sequence:split-sequence #\, update)) 
-                            (split-sequence:split-sequence #\Newline updates))))              
-         update-list))
-
-(find "47" (gethash "75" rule-table) :test #'equal)
-(gethash "75" rule-table)
-
-(let* ((updates (first update-list))
-       (key (car updates))
-       (subsequents (cdr updates))
-       (values (gethash key rule-table)))
-  (format t "Subs: ~A, values: ~A" subsequents values)
-  (subsetp subsequents values :test #'equal))
+(defun valid-updatep (rule-table update)
+  (let ((valid-update t))
+    (loop while update
+          do (let* ((key (car update))
+                    (subsequent-updates (cdr update))
+                    (subsequent-rules (gethash key rule-table)))
+               (unless (subsetp subsequent-updates subsequent-rules :test #'equal)
+                 (setf valid-update nil))
+               (setf update (cdr update))))
+    valid-update))
 
 (defun part1 (input)
-  ())
+  (let* ((rules (first (split-string-on-double-newline input)))
+         (updates (second (split-string-on-double-newline input)))
+         (rule-table (make-hash-table :test #'equal))
+         (update-list (mapcar (lambda (update)
+                                (split-sequence:split-sequence #\, update))
+                              (split-sequence:split-sequence #\Newline updates)))
+         (rolling-sum 0))
+    ;; add the rules to the hash table
+    (dolist (pair (mapcar (lambda (rule) 
+                            (split-sequence:split-sequence #\| rule)) 
+                          (split-sequence:split-sequence #\Newline rules)))
+      (push (cadr pair) (gethash (car pair) rule-table)))
+    ;; collect valid updates
+    (dolist (update update-list)
+      (let ((midpoint (elt update (floor (length update) 2))))
+        (when (and
+                (valid-updatep rule-table update) 
+                (/= 0 (length midpoint))) 
+          (incf rolling-sum (parse-integer midpoint)))))
+    rolling-sum))
+
+(equal 6951 (part1 (get-puzzle-input 2024 05 :single-string)))
--- src/2024/helpers.lisp
+++ src/2024/helpers.lisp
@@ -3,12 +3,14 @@
 ;(defparameter *session-token* (uiop:getenv "AOC_SESSION_TOKEN"))
 (defparameter *session-token* "53616c7465645f5fd91fa6ca57bf6db645585807c947bb09187e70733e9b0d8ad59788af016808d57dab7e78e2fb86dc084cea1a8505d242587976a083ca6152")
 
+(ql:quickload :dexador)
+
 (defun ensure-directory-exists (dir)
   "Ensure that the directory DIR exists, creating it if necessary."
   (unless (uiop:directory-exists-p dir)
     (ensure-directories-exist dir)))
 
-(ql:quickload :dexador)
+;;cib to edit within parens
 
 (defun fetch-puzzle-input (year day)
   "Fetch the puzzle input for the given YEAR and DAY from Advent of Code."
@@ -31,39 +33,36 @@
                 (uiop:split-string body :separator uiop:+lf+))
               (error "Failed to fetch input for year ~D, day ~D: ~A" year day '(body status)))))))
 
-(defun get-puzzle-input (year day &optional (type "string"))
+(defun get-puzzle-input (year day &optional (type :single-string))
   "Get the puzzle input for the given YEAR and DAY, downloading it if necessary."
   (let ((file-input (fetch-puzzle-input year day)))
     (case type
-      (:array  (to-array  file-input))
+      (:array  (to-array file-input))
       (:single-string (to-single-string file-input))
       (:2d-array (to-2d-array file-input)))))
 
 (defun to-single-string (input)
   "Parse a CONS of strings into a single string."
-  (format nil "~{~A~}" input))
+  (format nil "~{~A~%~}" input))
 
 (defun to-array (input)
   "Parse a multi-line string into an array of lines."
   (remove-if (lambda (line) (string= "" line))
-      (coerce 
-       (reduce 
-        (lambda (acc char)
-          (if (char= char #\Newline)
-              (cons "" acc)
-              (cons (concatenate 'string (first acc) (string char)) (rest acc)))) 
-        input :initial-value '(""))
-       'vector)))
+             (coerce 
+               (reduce 
+                 (lambda (acc char)
+                   (if (char= char #\Newline)
+                       (cons "" acc)
+                       (cons (concatenate 'string (first acc) (string char)) (rest acc)))) 
+                 input :initial-value '(""))
+               'vector)))
 
 (defun to-2d-array (input)
   (let* ((rows (length input))
          (cols (length (first input)))
          (matrix (make-array (list rows cols) :element-type 'character)))
-   (loop for i from 0 below rows
-         for line in input do
-         (loop for j from 0 below cols do
-               (setf (aref matrix i j) (aref line j))))
-   matrix))
-
-(let ((pair(split-sequence:split-sequence #\| "AB|CD")))
-  (cons (first pair) (second pair)))
+    (loop for i from 0 below rows
+          for line in input do
+          (loop for j from 0 below cols do
+                (setf (aref matrix i j) (aref line j))))
+    matrix))
